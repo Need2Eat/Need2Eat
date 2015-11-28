08df98e233f5934ee6fe5e11e4ceffa1db1d4370
André Mategka <mategka.andre@gmail.com>
2015-11-28 16:18:18 +0100

    Create a Butterknife binding for the Toolbar

--- app/src/main/java/at/need2eat/need2eat/MainActivity.java
+++ app/src/main/java/at/need2eat/need2eat/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-  
+
+  @Bind(R.id.toolbar) Toolbar toolbar;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    ButterKnife.bind(this);
     setSupportActionBar(toolbar);
   }
 
