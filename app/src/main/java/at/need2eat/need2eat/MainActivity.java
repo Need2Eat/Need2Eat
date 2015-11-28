33ec9133d3a6ed1d19d750763c0d8c02e4c15b71
André Mategka <mategka.andre@gmail.com>
2015-11-28 15:59:48 +0100

    Change ActionBar to Android Support Library Toolbar
    Outsource contents of activity_main.xml to content_main.xml
    Change activity_main.xml and styles.xml to use Android Support Library layouts and toolbar
    Add styles.xml for Android 21 to improve performance

--- app/src/main/java/at/need2eat/need2eat/MainActivity.java
+++ app/src/main/java/at/need2eat/need2eat/MainActivity.java
@@ -2,6 +2,7 @@ package at.need2eat.need2eat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
   }
 
   @Override
