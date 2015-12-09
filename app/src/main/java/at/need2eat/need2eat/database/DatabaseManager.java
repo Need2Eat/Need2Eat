0c445f236a337080f008bb634880e44ffd18c9c1
AM307 <mategka.andre@gmail.com>
2015-12-09 20:43:31 +0100

    Update documentation of the DatabaseManager interface

--- app/src/main/java/at/need2eat/need2eat/database/DatabaseManager.java
+++ app/src/main/java/at/need2eat/need2eat/database/DatabaseManager.java
@@ -9,19 +9,37 @@ import at.need2eat.need2eat.Product;
  */
 public interface DatabaseManager {
 
+  /**
+   * This method should return the {@link Product} with the specified ID by fetching it from the
+   * database.
+   * @param id the unique ID of the Product to fetch
+   * @return the Product stored in the database under the given ID
+   */
   Product getProduct(int id);
 
+  /**
+   * This method should return all {@link Product}s from the database at once.
+   * @return a {@link List} of all Products currently stored in the database
+   */
   List<Product> getAllProducts();
 
+  /**
+   * This method should insert the given {@link Product} into the database.
+   * @param newProduct the Product to be inserted
+   */
   void addProduct(Product newProduct);
 
   /**
-   * Information about the product (e.g. id of the Product to update) can be accessed through
-   * newProduct
-   * @param newProduct Contains the id of the Product to update, as well as its new properties
+   * This method should update the information of the given {@link Product} in the database with the
+   * data stored in it.
+   * @param newProduct the new Product data to replace existing values
    */
   void updateProduct(Product newProduct);
 
+  /**
+   * This method should delete the {@link Product} with the specified ID.
+   * @param id the unique ID of the Product to delete
+   */
   void deleteProduct(int id);
 
 }
