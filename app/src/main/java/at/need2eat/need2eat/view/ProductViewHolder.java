3b672954e844eca7174e7f98df82fe56d7cf178b
AM307 <mategka.andre@gmail.com>
2015-12-13 16:10:38 +0100

    Fix expiry date processing in ProductViewHolder

--- app/src/main/java/at/need2eat/need2eat/view/ProductViewHolder.java
+++ app/src/main/java/at/need2eat/need2eat/view/ProductViewHolder.java
@@ -4,6 +4,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import at.need2eat.need2eat.Product;
 import at.need2eat.need2eat.R;
 import butterknife.Bind;
@@ -17,6 +21,9 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
   @Bind(R.id.productName) protected TextView productNameView;
   @Bind(R.id.expirationDate) protected TextView expirationDateView;
   @Bind(R.id.indicator) protected IndicatorView indicatorView;
+
+  private static final Locale EXPIRY_DATE_LOCALE = Locale.GERMANY;
+  private static final String EXPIRY_DATE_FORMAT = "dd.mm.yyyy";
 
   private Product product;
 
@@ -35,7 +42,13 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
     this.product = product;
     indicatorView.update(3);
     productNameView.setText(product.getName());
-    expirationDateView.setText(product.getExpiryDate());
+    String expiryDate = getDateStringLocale(product.getExpiryDate());
+    expirationDateView.setText(expiryDate);
+  }
+
+  private String getDateStringLocale(Date date) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat(EXPIRY_DATE_FORMAT, EXPIRY_DATE_LOCALE);
+    return dateFormat.format(date);
   }
 
 }
