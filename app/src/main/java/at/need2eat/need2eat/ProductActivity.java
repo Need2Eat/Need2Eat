a024680dbb837c56b715f7288a72b3657e176f37
feistlsebastian <feistl.sebastian@gmail.com>
2015-12-16 12:23:42 +0100

    Add Delete Button in ProductActivity Add Button Listeners and Handlers in ProductActivity Fix Layoutproblems

--- app/src/main/java/at/need2eat/need2eat/ProductActivity.java
+++ app/src/main/java/at/need2eat/need2eat/ProductActivity.java
@@ -1,7 +1,11 @@
 package at.need2eat.need2eat;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -27,6 +31,50 @@ public class ProductActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_product);
     ButterKnife.bind(this);
+
+    intitializeLayout();
+  }
+
+  /**
+   * Function to initialize all important elements and applies onClickListeners to the buttons.
+   */
+  private void intitializeLayout() {
+    final LinearLayout BACK_BUTTON = (LinearLayout)findViewById(R.id.backButton);
+    final LinearLayout DELETE_BUTTON = (LinearLayout)findViewById(R.id.deleteButton);
+    final LinearLayout EDIT_BUTTON = (LinearLayout)findViewById(R.id.editButton);
+
+    BACK_BUTTON.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+
+    DELETE_BUTTON.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // TODO: delete product from database
+        finish();
+      }
+    });
+
+    EDIT_BUTTON.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Resources resources = getResources();
+        final String GTIN = resources.getString(R.string.extra_gtin);
+        final String EXPIRY_DATE = resources.getString(R.string.extra_expiry);
+        final String NAME = resources.getString(R.string.extra_name);
+        Intent intent = new Intent(ProductActivity.this, EditActivity.class);
+        intent.putExtra(GTIN, "48495384");
+        intent.putExtra(EXPIRY_DATE, "12.12.2017");
+        intent.putExtra(NAME, "HansHandcreme");
+        intent.putExtra(resources.getString(R.string.extra_id), 112);
+        ProductActivity.this.startActivity(intent);
+        finish();
+      }
+    });
+
   }
 
   /**
