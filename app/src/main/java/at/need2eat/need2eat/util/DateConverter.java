0631737bd14d509ac64c4b64fe61494ef9e346a6
Maxi-Nothnagel <mx.nothnagel@gmail.com>
2015-12-18 00:29:37 +0100

    Add javadoc to DateConverter

--- app/src/main/java/at/need2eat/need2eat/util/DateConverter.java
+++ app/src/main/java/at/need2eat/need2eat/util/DateConverter.java
@@ -12,11 +12,23 @@ import java.util.Locale;
  */
 public class DateConverter {
 
+  /**
+   * Get a {@link Date} from a {@code String}
+   * @param expiryDate The {@code String} that should be converted to a {@code Date}
+   * @return The converted {@code Date}
+   * @throws ParseException if the {@code String} could not be converted
+   */
   public static Date getDateFromString(String expiryDate) throws ParseException {
     SimpleDateFormat sdf = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMANY);
     return sdf.parse(expiryDate);
   }
 
+  /**
+   * Get a {@code String} from a {@link Date}
+   * @param expiryDate The {@code Date} that should be converted to a {@code String}
+   * @return The converted {@code String}
+   * @throws IllegalArgumentException if the given {@code Date} is null
+   */
   public static String getStringFromDate(Date expiryDate) throws IllegalArgumentException {
     if (expiryDate == null) {
       throw new IllegalArgumentException();
