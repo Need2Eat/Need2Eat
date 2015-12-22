6a86624e26be1dc4cd1d41704216057e13c9ec45
AM307 <mategka.andre@gmail.com>
2015-12-22 19:43:58 +0100

    Add Javadoc documentation to AdapterActivity

--- app/src/main/java/at/need2eat/need2eat/AdapterActivity.java
+++ app/src/main/java/at/need2eat/need2eat/AdapterActivity.java
@@ -5,10 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import java.util.List;
 
 /**
- * Created by AM307 on 22.12.2015.
+ * This class represents an {@link AppCompatActivity} with extended functionality. The contents of
+ * Activities that inherit from this class can be reloaded by calling {@link #refreshAdapter(List)}.
+ * Type T represents the data type of the values that are needed to perform this action.
+ * @author AM307
  */
 public abstract class AdapterActivity<T> extends AppCompatActivity {
 
+  /**
+   * Overriding methods should implement behaviour that reloads the contents of the inheriting
+   * Activity using the list of items given.
+   * @param items the list of items that should be used to reload the Activity's contents
+   */
   public abstract void refreshAdapter(List<T> items);
 
 }
