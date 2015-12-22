44d8eafc1e90f9f784616753664415a999d19c78
Maxi-Nothnagel <mx.nothnagel@gmail.com>
2015-12-22 08:24:51 +0100

    Add javadoc to DatabaseTask class

--- app/src/main/java/at/need2eat/need2eat/database/DatabaseTask.java
+++ app/src/main/java/at/need2eat/need2eat/database/DatabaseTask.java
@@ -5,7 +5,8 @@ import android.content.Context;
 import at.need2eat.need2eat.Product;
 
 /**
- *
+ * This class is used to access the local database asynchronously and either insert, delete or
+ * update a {@link Product}
  * @author Maxi Nothnagel - mx.nothnagel@gmail.com
  */
 public class DatabaseTask implements Runnable {
@@ -14,6 +15,12 @@ public class DatabaseTask implements Runnable {
   private DatabaseHandler handler;
   private boolean delete;
 
+  /**
+   * Creates a new DatabaseTask {@code Thread}
+   * @param product the {@code Product} you want to update, delete or insert in the database
+   * @param context the {@code Context} to use, create or open the database
+   * @param delete {@code true} if the given {@code Product} should be deleted from the database
+   */
   public DatabaseTask(Product product, Context context, boolean delete) {
     this.product = product;
     handler = new DatabaseHandler(context);
